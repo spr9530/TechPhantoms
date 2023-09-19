bdda5ba12f26291cf010e45346693e3213aac4e5
spr9530 <145482692+spr9530@users.noreply.github.com>
2023-09-20 00:06:59 +0530

    Add files via upload

---
+++ config/fireBase.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDj37fEI7NQK6-Dx0xAyMMqGnhgNZ3QJXo",
+  
+    authDomain: "techphantoms-b37ec.firebaseapp.com",
+  
+    projectId: "techphantoms-b37ec",
+  
+    storageBucket: "techphantoms-b37ec.appspot.com",
+  
+    messagingSenderId: "607579087651",
+  
+    appId: "1:607579087651:web:1b1997b368feb6d519e296",
+  
+    measurementId: "G-907WNGFGMY"
+  
+  };
+  
+  module.exports = firebaseConfig;
